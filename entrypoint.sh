a188e6ce00d0e75fbe4dc219eb77fb24e1ace109
Miguel Ángel Pintor Moral <miwe08@gmail.com>
2020-08-21 18:42:55 +0200

    Enforced credentials refresh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,19 @@
 
 set -e
 
-if [ ! -d "$HOME/.config/gcloud" ]; then
-   if [ -z "${APPLICATION_CREDENTIALS-}" ]; then
-      echo "APPLICATION_CREDENTIALS not found. Exiting...."
-      exit 1
-   fi
-
-   if [ -z "${PROJECT_ID-}" ]; then
-      echo "PROJECT_ID not found. Exiting...."
-      exit 1
-   fi
-
-   echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
-
-   gcloud auth activate-service-account --key-file=/tmp/account.json --project "$PROJECT_ID"
-
+if [ -z "${APPLICATION_CREDENTIALS-}" ]; then
+   echo "APPLICATION_CREDENTIALS not found. Exiting...."
+   exit 1
 fi
+
+if [ -z "${PROJECT_ID-}" ]; then
+   echo "PROJECT_ID not found. Exiting...."
+   exit 1
+fi
+
+echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
+
+gcloud auth activate-service-account --key-file=/tmp/account.json --project "$PROJECT_ID"
 
 echo ::add-path::/google-cloud-sdk/bin/gcloud
 echo ::add-path::/google-cloud-sdk/bin/gsutil
