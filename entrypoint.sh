8dfea40e3bb08d22dda706a0783b00b28e1830af
Miguel Ángel Pintor Moral <miwe08@gmail.com>
2020-10-09 18:11:35 +0200

    Removed deprecated ::add-path::

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,8 +20,12 @@ echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
 
 gcloud auth activate-service-account --key-file=/tmp/account.json --project "$PROJECT_ID"
 
-echo ::add-path::/google-cloud-sdk/bin/gcloud
-echo ::add-path::/google-cloud-sdk/bin/gsutil
+# TODO: add these paths to the runner if we want to share the auth
+#echo ::add-path::/google-cloud-sdk/bin/gcloud
+#echo ::add-path::/google-cloud-sdk/bin/gsutil
+# maybe???:
+# echo "{/google-cloud-sdk/bin/gcloud}" >> $GITHUB_PATH
+# echo "{/google-cloud-sdk/bin/gsutil}" >> $GITHUB_PATH
 
 # Update kubeConfig.
 gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$ZONE_NAME" --project "$PROJECT_ID"
